162e732649e745770989355826420ec7ef81860c
Rafif Putra <133955488+boboiboy777@users.noreply.github.com>
2024-12-27 21:53:15 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Typewriter } from "react-simple-typewriter";
 import Countdown from "react-countdown";
 
 function App() {
-  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023👋"]);
+  const [newYearMessage, setNewYearMessage] = useState(["Bye 2024👋"]);
 
   const particlesInit = async (preset) => {
     await loadFireworksPreset(preset);
